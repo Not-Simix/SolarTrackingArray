2036c9f86112def4172443e543300fb14b6e2e29
Not-Simix <124903630+Not-Simix@users.noreply.github.com>
2023-03-02 20:41:21 -0500

    Update DCMotorRotaryEncoder_Full.cpp
    Updating comments and syntax for ease of use and readability

--- DCMotorRotaryEncoder_Full.cpp
+++ DCMotorRotaryEncoder_Full.cpp
@@ -1,23 +1,22 @@
 #define GPSReceive Serial1
+
 const int ENB_PIN = 7;
 const int IN3_PIN = 6;
 const int IN4_PIN = 5;
-
 const int encoderA = 2;
 const int encoderB = 3;
-volatile int encoderPos = 0;
 
+volatile int encoderPos = 0;
 int targetValueAzimuth = 0;
 
 void setup() {
   Serial.begin(115200);
   GPSReceive.begin(115200);
-  // set motor pins
+
   pinMode(ENB_PIN, OUTPUT);
   pinMode(IN3_PIN, OUTPUT);
   pinMode(IN4_PIN, OUTPUT);
 
-  // set encoder pins
   pinMode(encoderA, INPUT_PULLUP);
   pinMode(encoderB, INPUT_PULLUP);
 
@@ -50,7 +49,7 @@ void loop() {
 
   // Calculate error between target position and current position
   if (encoderPos < targetValueAzimuth - 5){
-    CWDC();
+    CWDC(); 
   } else if (encoderPos > targetValueAzimuth + 5){
     CCWDC();
   } else{
@@ -58,9 +57,9 @@ void loop() {
   }
   
   // Print current position and target position to serial monitor
-  Serial.print("Current position: ");
+  Serial.print("Current position ");
   Serial.print(encoderReading);
-  Serial.print("   Target position: ");
+  Serial.print(" and the Target position is ");
   Serial.println(targetValueAzimuth);
 }
 
