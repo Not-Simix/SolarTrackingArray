f7709a93472f7aea1c5fb1545a6de0c28014705d
Not-Simix <124903630+Not-Simix@users.noreply.github.com>
2023-03-02 20:32:30 -0500

    Update Anemometer_Full.cpp
    Updated comments and syntax to allow for easier reading and understanding.

--- Anemometer_Full.cpp
+++ Anemometer_Full.cpp
@@ -1,20 +1,17 @@
 #define TransmitActuator Serial1
-int AnemData = 0;
 
+int AnemData = 0;
 bool isCounting = false;
-bool isCountingSecondary = false;
+bool isCountingSecondary = falseh;
 unsigned long startTime = 0;
 unsigned long currTime = millis();
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   TransmitActuator.begin(115200);
 }
 
 void loop() {
-  // put your main code here to run repeatedly:
-
   float sensorValue = analogRead(A0);
   Serial.print("Analog Value =");
   Serial.println(sensorValue);
@@ -34,35 +31,34 @@ void loop() {
   Serial.println("mph");
   Serial.print(speed_knot);
   Serial.println("knots");
-
   Serial.println(" ");
-  // delay(1000);
+  
   if (!isCounting && speed_knot > 35) { //Speed hits 35 for the first time
-      isCounting = true;
+      isCounting = true; // flags begin feathering
       startTime = currTime;
   }
 
    if (currTime > startTime + 15000 && isCounting) { //Time threshold has been reached
-      AnemData = 0;
-      isCounting = false;
+      AnemData = 0; // Activates feather
+      isCounting = false; // stops flag to begin feathering
    }
 
    if (speed_knot < 35 && currTime < startTime + 15000 && isCounting) { //It's been less than 15 seconds and our speed has reduced
-      isCounting = false;
+      isCounting = false; // stops flag to begin feathering
    }
 
   if (speed_knot < 30 && AnemData == 0 && !isCountingSecondary) { //Speed dips below 30 for the first time after being at 35
-      isCountingSecondary = true;
+      isCountingSecondary = true; // flags end feathering
       startTime = currTime;
   }
 
   if (currTime > startTime + 600000 && isCountingSecondary) { //Time threshold has been reached
-      AnemData = 1;
-      isCountingSecondary = false;
+      AnemData = 1; // Activates normal functioning
+      isCountingSecondary = false; // stops flag to end feathering
   }
 
   if (speed_knot > 30 && currTime < startTime + 600000 && isCountingSecondary) { //It's been less than 10 minutes and our speed has increased
-      isCountingSecondary = false;
+      isCountingSecondary = false; // stops flag to end feathering
   }
 
   TransmitActuator.write(AnemData);
