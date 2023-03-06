f850a179928fe9f15a5f6e3998b03e65cd07e09f
Not-Simix <124903630+Not-Simix@users.noreply.github.com>
2023-03-06 11:45:57 -0500

    Update ActuatorInclinometer_Full.cpp

--- ActuatorInclinometer_Full.cpp
+++ ActuatorInclinometer_Full.cpp
@@ -13,20 +13,20 @@ const int IN2_PIN = 5;
 
 //Create the target value
 int targetValueElevation = 0;
+int AnemData = 1;
+int targetVal;
 
 void setup() {
   Serial.begin(115200);
-  GPSReceive.begin(115200);
+  GPSReceive.begin(19200);
   AnemometerReceive.begin(115200);
   pinMode(ENA_PIN, OUTPUT);
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
 
   digitalWrite(ENA_PIN, HIGH);
-
-  Serial.println("Initialize MPU6050");
-
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
+  Serial.println("Begin Actuator code");
+    while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
     Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
     delay(500);
   }
@@ -48,25 +48,46 @@ void extendActuator() {
 }
 
 void loop() {
-  
-  AnemData = AnemometerReceive.read(); //reads whether feathering needs to take place
-
-  if (AnemData = 1) { // normal functioning
-    targetValueElevation = GPSReceive.read();
-  } else { // Feather mode when AnemData = 0
-    targetValueElevation = 0
+  if (AnemometerReceive.available() >= 2){
+    AnemData = AnemometerReceive.read(); //reads whether feathering needs to take place
   }
-  
+  if (GPSReceive.available() >= 2  ){
+    targetVal = GPSReceive.read();
+  }
+  Serial.println(AnemData);
+  Serial.print("Target Val: ");
+  Serial.println(targetVal);
+  // Uncomment to debug
+  // Serial.println("Anem Data read complete");
+  if (AnemData == 1) { // normal functioning
+   targetValueElevation = targetVal;
+    } else { // Feather mode when AnemData = 0
+    targetValueElevation = 0;
+  }
+  if (targetValueElevation > 90) {
+    targetValueElevation = 0;
+  } else { 
+    targetValueElevation = targetValueElevation;
+  }
+  // Uncomment to debug
+  // Serial.println("Anem Data argument complete");
   // Read normalized values 
-  Vector normAccel = mpu.readNormalizeAccel();
-
-  // Calculate Solar Panel Elevation
+Vector normAccel = mpu.readNormalizeAccel();
+// Calculate Solar Panel Elevation
   int elevation = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
  
+ // Uncomment to debug
+  // Serial.println("elevation of solar panel calculated");
+
   // Output
-  Serial.print("Elevation = ");
-  Serial.println(elevation);
-  
+Serial.print("Elevation = ");
+Serial.print(elevation);
+Serial.print(", Target value elevation = ");
+Serial.print(targetValueElevation);
+Serial.print(", AnemData = ");
+Serial.println(AnemData);
+
+
   //The following determines if the Solar panel is within +- 3 degrees of the suns elevation
   if (elevation < targetValueElevation - 3) {
     extendActuator();
