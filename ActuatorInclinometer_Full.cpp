a4e138048e56e9ee833a754ce19f196d05a24322
Not-Simix <124903630+Not-Simix@users.noreply.github.com>
2023-03-29 16:07:07 -0400

    Update ActuatorInclinometer_Full.cpp
    Updating to functional code

--- ActuatorInclinometer_Full.cpp
+++ ActuatorInclinometer_Full.cpp
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <MPU6050.h>
+#include <LiquidCrystal.h>
 
 #define GPSReceive Serial1
 #define AnemometerReceive Serial2
@@ -10,7 +11,8 @@ MPU6050 mpu;
 const int ENA_PIN = 7;
 const int IN1_PIN = 6;
 const int IN2_PIN = 5;
-
+const int rs = 22, en = 23, d24 = 24, d25 = 25, d26 = 26, d27 = 27;
+LiquidCrystal lcd(rs, en, d24, d25, d26, d27);
 //Create the target value
 int targetValueElevation = 0;
 int AnemData = 1;
@@ -20,9 +22,11 @@ void setup() {
   Serial.begin(115200);
   GPSReceive.begin(19200);
   AnemometerReceive.begin(115200);
+  lcd.begin(16,2);
   pinMode(ENA_PIN, OUTPUT);
   pinMode(IN1_PIN, OUTPUT);
   pinMode(IN2_PIN, OUTPUT);
+  lcd.print("TargetE  ActualE");
 
   digitalWrite(ENA_PIN, HIGH);
   Serial.println("Begin Actuator code");
@@ -53,15 +57,17 @@ void loop() {
   }
   if (GPSReceive.available() >= 2  ){
     targetVal = GPSReceive.read();
+    //targetVal = 0;
   }
+
   Serial.println(AnemData);
   Serial.print("Target Val: ");
   Serial.println(targetVal);
   // Uncomment to debug
   // Serial.println("Anem Data read complete");
   if (AnemData == 1) { // normal functioning
-   targetValueElevation = targetVal;
-    } else { // Feather mode when AnemData = 0
+    targetValueElevation = targetVal;
+  } else { // Feather mode when AnemData = 0
     targetValueElevation = 0;
   }
   if (targetValueElevation > 90) {
@@ -74,7 +80,7 @@ void loop() {
   // Read normalized values 
 Vector normAccel = mpu.readNormalizeAccel();
 // Calculate Solar Panel Elevation
-  int elevation = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI;
+  int elevation = abs(-(atan2(normAccel.XAxis, sqrt(normAccel.YAxis*normAccel.YAxis + normAccel.ZAxis*normAccel.ZAxis))*180.0)/M_PI);
  
  // Uncomment to debug
   // Serial.println("elevation of solar panel calculated");
@@ -86,13 +92,16 @@ Serial.print(", Target value elevation = ");
 Serial.print(targetValueElevation);
 Serial.print(", AnemData = ");
 Serial.println(AnemData);
+lcd.setCursor(0,1);
+lcd.print(targetValueElevation);
+lcd.setCursor(9,1);
+lcd.print(elevation);
 
-
-  //The following determines if the Solar panel is within +- 3 degrees of the suns elevation
-  if (elevation < targetValueElevation - 3) {
+  //The following determines if the Solar panel is within +- 5 degrees of the suns elevation
+  if (elevation < targetValueElevation - 5) {
     extendActuator();
   }
-  else if (elevation > targetValueElevation + 3) {
+  else if (elevation > targetValueElevation + 5) {
     retractActuator();
   }
   else{
